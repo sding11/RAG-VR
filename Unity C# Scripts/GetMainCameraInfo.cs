26f391b7232c38f85d7b31a24a89d6defc211095
sding11 <shiyiddd@gmail.com>
2025-02-06 14:31:31 -0500

    revise

---
+++ Unity C# Scripts/GetMainCameraInfo.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class GetMainCameraInfo : MonoBehaviour
+{
+    // define a class to store Camera information
+    [System.Serializable]
+    public class CameraInfo
+    {
+        public Vector3 position;
+        public Quaternion rotation;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // require mainCamera information
+        Camera mainCamera = Camera.main;
+
+        if (mainCamera != null)
+        {
+            // create CameraInfo object
+            CameraInfo cameraInfo = new CameraInfo
+            {
+                position = mainCamera.transform.position,
+                rotation = mainCamera.transform.rotation
+            };
+
+            // change Camera information series to JSON
+            string json = JsonUtility.ToJson(cameraInfo, true);
+
+            // define save route
+            string directoryPath = ".\\scene_information";
+            string filePath = directoryPath + "\\player.json";
+
+
+
+            // write JSON to documents
+            File.WriteAllText(filePath, json);
+
+            // output save route
+            Debug.Log($"main CameraInfo has save to: {filePath}");
+        }
+        else
+        {
+            Debug.LogError("No mainCamera found!");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Unity C# Scripts/PlayerController.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 5f;       // player move speed
+    public float rotationSpeed = 100f; // player rotation Speed
+    public Transform cameraTransform; // camera Transform
+
+    void Update()
+    {
+        MovePlayer();
+        RotatePlayer();
+    }
+
+    void MovePlayer()
+    {
+        // Get the up and down arrow (W/S or ↑/↓) Input to move forward and backward
+        float moveZ = Input.GetAxis("Vertical");
+
+        // Get the camera orientation
+        Vector3 forward = cameraTransform.forward;
+        Vector3 right = cameraTransform.right;
+
+        // Keep moving horizontally (ignoring the Y component)
+        forward.y = 0;
+        right.y = 0;
+        forward.Normalize();
+        right.Normalize();
+
+        // Calculated direction of movement
+        Vector3 moveDirection = forward * moveZ;
+
+        // move player
+        transform.Translate(moveDirection * moveSpeed * Time.deltaTime, Space.World);
+    }
+
+    void RotatePlayer()
+    {
+        // Get left and right arrows (A/D or ←/→) Input for horizontal rotation
+        float rotationInput = Input.GetAxis("Horizontal");
+
+        // Calculated rotation Angle
+        float rotation = rotationInput * rotationSpeed * Time.deltaTime;
+
+        // Rotate the player around the Y axis
+        transform.Rotate(Vector3.up * rotation);
+    }
+}
